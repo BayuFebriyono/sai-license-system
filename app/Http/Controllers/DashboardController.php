d904977802674e447efc65cf6e5ca39a40763d1e
BayuFebriyono <bayu.f.p.09@gmail.com>
2022-03-22 20:49:10 +0700

    Memartisi Tampilan Dashboard Menjadi Navbar Dan Main

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.main');
+        return view('user.list_peserta');
     }
 }
---
+++ resources/views/user/list_peserta.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.main')
+@section('content')
+
+<h1>Tes Halo</h1>
+@endsection
